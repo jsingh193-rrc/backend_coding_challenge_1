39cd2ee17dc204a171022c7410c8e76ad92b391a
Jashandeep Singh <jsingh193@academic.rrc.ca>
2026-01-19 08:36:27 -0600

    Add players data into playerModel file.

--- src/api/v1/models/playerModel.ts
+++ src/api/v1/models/playerModel.ts
@@ -1,7 +1,31 @@
-
-interface Player {    id: number;
+export interface Player {
+    id: number;
     name: string;
     wins: number;
     losses: number;
     totalScore: number;
 }
+
+export const players: Player[] = [
+    {
+        id: 1,
+        name: "ShadowStrike",
+        wins: 15,
+        losses: 5,
+        totalScore: 28500
+    },
+    {
+        id: 2,
+        name: "NoobMaster",
+        wins: 3,
+        losses: 12,
+        totalScore: 4200
+    },
+    {
+        id: 3,
+        name: "ProGamer99",
+        wins: 0,
+        losses: 0,
+        totalScore: 0
+    }
+];
